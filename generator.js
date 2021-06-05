6cc13660b84565c72faff64e23faff0d100acb2c
PiyanshK <70416022+PiyanshK@users.noreply.github.com>
2021-06-05 11:05:35 -0700

    recommit

--- generator.js
+++ generator.js
@@ -1,2 +1,16 @@
-const txtgen = require('txtgen');
+const textholder = document.getElementById('generatedtext');
+const btnscentence = document.getElementById('generatesentence');
+const btnparagraph = document.getElementById('generateparagraph');
+const btnarticle = document.getElementById('genereatearticle');
+const txtgen = txtgenfunc(); 
 
+
+function clearContents(element) {
+    element.value = '';
+  }
+
+document.getElementById('generatesentence').addEventListener("click", function(){
+    clearContents(textholder);
+    //let scentence = textgen.scentence;
+    console.log("scentence");
+})
