1024010c6a74caa27cd1871e5dc6b5836690b197
PiyanshK <70416022+PiyanshK@users.noreply.github.com>
2021-06-07 21:44:50 -0700

    Changing text / New style

--- src/scripts/generator.js
+++ src/scripts/generator.js
@@ -1,36 +1,40 @@
-const animate = require('animejs');
 const text = document.getElementById("text");
 const txtgen = require("txtgen");
+const container = document.querySelector("container");
 
-
-var sentence = txtgen.sentence();
-var paragraph = txtgen.paragraph();
-var article = txtgen.article();
-
-animate({
-    targets: 'body',
-    duration: 5000,
-    background: "#001375",
-})
 
 document
   .getElementById("generateparagraph")
   .addEventListener("click", function () {
+    var paragraph = txtgen.paragraph();
     text.innerHTML = paragraph;
   });
 
 document
   .getElementById("generatesentence")
   .addEventListener("click", function () {
+    var sentence = txtgen.sentence();
     text.innerHTML = sentence;
   });
 
 document
   .getElementById("generatearticle")
   .addEventListener("click", function () {
+    var article = txtgen.article();
     text.innerHTML = article;
   });
 
-var sentence = txtgen.sentence();
-var paragraph = txtgen.paragraph();
-var article = txtgen.article();
+  const animate = require("animejs");
+
+  animate({
+      targets: "body",
+      duration: 10000,
+      background: "#e80202",
+    })
+  
+  
+  for (var i = 0; 1 <= 100; i++){ 
+      const blocks = document.createElement('div');
+      blocks.classList.add('block');
+      container.appendChild(blocks);
+  }
