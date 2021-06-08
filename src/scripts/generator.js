1c90b47611505615535cada1ca34ba3900403b48
PiyanshK <70416022+PiyanshK@users.noreply.github.com>
2021-06-07 22:08:47 -0700

    final deign

--- src/scripts/generator.js
+++ src/scripts/generator.js
@@ -1,7 +1,8 @@
+// Text Gen
+
 const text = document.getElementById("text");
 const txtgen = require("txtgen");
 const container = document.querySelector("container");
-
 
 document
   .getElementById("generateparagraph")
@@ -24,17 +25,11 @@ document
     text.innerHTML = article;
   });
 
-  const animate = require("animejs");
+// Animations
 
-  animate({
-      targets: "body",
-      duration: 10000,
-      background: "#e80202",
-    })
+const animate = require("animejs");
+
+animate({
+  target: "body",
   
-  
-  for (var i = 0; 1 <= 100; i++){ 
-      const blocks = document.createElement('div');
-      blocks.classList.add('block');
-      container.appendChild(blocks);
-  }
+})
